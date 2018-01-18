92b7f8b53df24b496238138c884c3bf56f00a2cf
DESKTOP-UF5TNQ4\kjw90 <rhdqud7296@naver.com>
2018-01-18 12:34:41 +0900

    테스트3

--- src/main/java/com/example/spring/personnel/controller/ScheduleController.java
+++ src/main/java/com/example/spring/personnel/controller/ScheduleController.java
@@ -17,6 +17,7 @@ public class ScheduleController {
 	//private String PRE_VIEW_PATH = "personnel/schedule/";
 	
 	//일정보기
+	//한글테스트2
 	@RequestMapping(value="scheduleView.do")
 	public String scheduleView() {
 		
